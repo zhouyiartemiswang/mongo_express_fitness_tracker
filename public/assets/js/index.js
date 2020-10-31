ec89414de4c4ce2d9ad52133989cf95f7756e49a
Artemis <wzyzhouyi@gmail.com>
2020-10-30 19:11:29 -0700

    added forms for cardio and resistance training

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
-    $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
+    // $(`#myModal`).on(`shown.bs.modal`, function () {
+    //     $(`#myInput`).trigger('focus');
+    // })
+    console.log("check");
+    $("#resistanceForm").hide();
+    $("#cardioForm").hide();
+    $("#training-type").change(function () {
+        const type = $("#training-type").val();
+        if (type === "Cardio") {
+            $("#cardioForm").show();
+            $("#resistanceForm").hide();
+        } else {
+            $("#cardioForm").hide();
+            $("#resistanceForm").show();
+        }
     })
 })
